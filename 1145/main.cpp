3226296d86ca2245f4b0a3be78e8e34bd1270859
Makuhin Andrey <a_makuhim@mail.ru>
2018-12-05 02:02:11 +0500

    слегка улучшенное(усложненое) решение

--- 1145/main.cpp
+++ 1145/main.cpp
@@ -16,14 +16,22 @@ struct cell
 {
     int x;
     int y;
+    bool operator ==(cell other)
+    {
+        return this->x == other.x && this->y == other.y;
+    }
 };
 
 int n, m;
+cell GSTART{0,0};
+bool startFounded = false;
 
 vector<char> labirint;
 vector<bool> msk;
+vector<bool> msk_find;
 
 int GetLenghtRope(cell next, int len);
+cell FindStart(cell next);
 
 int main() {
     
@@ -40,15 +48,17 @@ int main() {
     {
         labirint.push_back(input);
         msk.push_back(false);
+        msk_find.push_back(false);
     }
     
-    cell start{0,0};
+    cell pre_start{0,0};
     bool isStart = false;
     for(int i = 0; i < m - 1; i++)
     {
         for(int j = 0; j < n - 1; j++)
         {
-            start = {j, i};
+            
+            pre_start = {j, i};
             if(labirint[i * n + j] == '.')
             {
                 isStart = true;
@@ -59,7 +69,12 @@ int main() {
             break;
     }
     
-    int result = GetLenghtRope(start, 0);
+    cell dummy = FindStart(pre_start);
+    //cout << pre_start.x << " " << pre_start.y << endl;
+    //cout << GSTART.x << " " << GSTART.y << endl;
+    
+    
+    int result = GetLenghtRope(GSTART, 0);
     
     cout << result;
     
@@ -74,8 +89,6 @@ int GetLenghtRope(cell next, int lenght)
     if(labirint[next.y * n + next.x] == '#' || msk[next.y * n + next.x] == true)
         return 0;
     
-    
-    
     msk[next.y * n + next.x] = true;
     
     cell dir[] = {  {next.x + 1, next.y},
@@ -89,10 +102,10 @@ int GetLenghtRope(cell next, int lenght)
     int up = GetLenghtRope(dir[2], lenght+1);
     int down = GetLenghtRope(dir[3], lenght+1);
     
-    if(right > lenght) right -= lenght;
-    if(left > lenght) left -= lenght;
-    if(up > lenght) up -= lenght;
-    if(down > lenght) down -= lenght;
+    if(right > 0) right -= lenght;
+    if(left > 0) left -= lenght;
+    if(up > 0) up -= lenght;
+    if(down > 0) down -= lenght;
     
     int lenghts[5] = { lenght, right, left, up, down};
     
@@ -110,5 +123,45 @@ int GetLenghtRope(cell next, int lenght)
     }
     
     return lenghts[0] + lenghts[1];
+    
+}
+
+cell FindStart(cell next)
+{
+    cell dir[] = {  {next.x + 1, next.y},
+        {next.x - 1, next.y},
+        {next.x, next.y - 1},
+        {next.x, next.y + 1}
+    };
+    
+    if(next.x < 0 || next.x >= n || next.y < 0 || next.y >= m)
+        return next;
+    
+    if(labirint[next.y * n + next.x] == '#' || msk_find[next.y * n + next.x] == true)
+        return next;
+    
+    msk_find[next.y * n + next.x] = true;
+    
+    if ((labirint[dir[0].y*n + dir[0].x] == '#' || msk_find[dir[0].y * n + dir[0].x] == true) &&
+        (labirint[dir[1].y*n + dir[1].x] == '#' || msk_find[dir[1].y * n + dir[1].x] == true) &&
+        (labirint[dir[2].y*n + dir[2].x] == '#' || msk_find[dir[2].y * n + dir[2].x] == true) &&
+        (labirint[dir[3].y*n + dir[3].x] == '#' || msk_find[dir[3].y * n + dir[3].x] == true) &&
+        !startFounded)
+    {
+        GSTART = next;
+        startFounded = true;
+    }
+    
+    cell right = FindStart(dir[0]);
+    cell left = FindStart(dir[1]);
+    cell up = FindStart(dir[2]);
+    cell down = FindStart(dir[3]);
+    
+    /*if (right == next && left == next && up == next && down == next && !startFounded)
+    {
+        GSTART = next;
+        startFounded = true;
+    }*/
+    return {-1, -1};
     
 }
