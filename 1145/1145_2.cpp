c89ba334073c1d65d0c85cee341abc6a729dbb15
Makuhin Andrey <a_makuhim@mail.ru>
2018-12-06 02:06:28 +0500

    решение есть, но вылез за рамки времени

--- 1145/1145_2.cpp
+++ 1145/1145_2.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <stdio.h>
+#include <queue>
 
 using namespace std;
 
@@ -16,14 +17,25 @@ struct cell
 	}
 };
 
+struct stepCell
+{
+    cell point;
+    int step;
+};
+
 int n, m;
+int cellsCount = 0;
 vector<char> labirint;
 vector<int> maskI;
 vector<bool> maskB;
 vector<cell> tupiks;
+queue<stepCell> myQ;
+
+vector<vector<int>> grafTupik;
 
 int FindTupiks(cell next);
 
+int CalculateDistance(cell start, cell end);
 
 int main()
 {
@@ -35,39 +47,61 @@ int main()
 	cin >> n >> m;	
 
 	char input;
-	while (cin >> input)
+	while (cin >> input)//получаем ввод
 	{
+        if(input == '.')
+            cellsCount++;
 		labirint.push_back(input);
 		maskI.push_back(-1);
 		maskB.push_back(false);
 	}
 
 	cell startFind{1,1};
-	for (int y = 1; y < m - 1; y++)
+    bool finded = false;
+	for (int y = 1; y < m - 1; y++)//ищем стартовую точку
 	{
 		for (int x = 1; x < n - 1; x++)
 		{
 			startFind = { x,y };
 			if (labirint[y * n + x] == '.')
 			{
+                finded = true;
 				break;
 			}
 		}
+        if(finded) break;
 	}
-
-	int tup_count = FindTupiks(startFind);	
-
-	cout << "tupiks " << tup_count << endl;
-			
-	for (cell c : tupiks)
-	{
-		cout << c.x << " " << c.y << endl;
-	}
-
-
-	system("Pause");
-
-
+    
+	int tup_count = FindTupiks(startFind);
+    
+    for(int x = 0; x < tupiks.size(); x++)
+    {
+        grafTupik.push_back(vector<int>());
+        for(int y = 0; y < tupiks.size(); y++)
+        {
+            grafTupik[x].push_back(0);
+        }
+    }
+    
+    int maxLenght = 0;
+    for (int start = 0; start < tupiks.size(); start++)
+    {
+        for (int end = 0; end < tupiks.size(); end++)
+        {
+            if(start != end)
+            {
+                int lenght = CalculateDistance(tupiks[start], tupiks[end]);
+                grafTupik[start][end] = lenght;
+                grafTupik[end][start] = lenght;
+                if(lenght > maxLenght)
+                {
+                    maxLenght = lenght;
+                }
+            }
+        }
+    }
+    
+    cout << maxLenght;
 
 	return 0;
 }
@@ -85,16 +119,62 @@ int FindTupiks(cell next)
 	int right = FindTupiks({ next.x + 1, next.y });
 	int up = FindTupiks({ next.x, next.y - 1 });
 	int down = FindTupiks({ next.x, next.y + 1 });
-
-	int cellVal = 1;
+	
 	int continues = left + right + up + down;
 
-	if (continues == 0)
+	if (continues == 0 || continues == cellsCount - 1)
 	{
 		tupiks.push_back(next);
 	}
 
-	return cellVal + continues;
+	return continues + 1;
+}
+
+int CalculateDistance(cell start, cell end)
+{
+    for (int y = 0; y < m; y++)
+    {
+        for (int x = 0; x < n; x++)
+        {
+            maskI[y * n + x] = -1;
+        }
+    }
+
+    myQ.push({start, 0});
+    int step = 0;
+    
+    while(!myQ.empty())
+    {
+        cell recent = myQ.front().point;
+        int step = myQ.front().step;
+        
+        if(labirint[recent.y * n + recent.x] == '#' || maskI[recent.y * n + recent.x] > 0)
+        {
+            myQ.pop();
+            continue;
+        }
+        
+        maskI[recent.y * n + recent.x] = step;
+        
+        if(recent == end)
+        {
+            break;
+        }
+        
+        myQ.push({{recent.x + 1, recent.y}, step + 1});
+        myQ.push({{recent.x - 1, recent.y}, step + 1});
+        myQ.push({{recent.x, recent.y + 1}, step + 1});
+        myQ.push({{recent.x, recent.y - 1}, step + 1});
+       
+        myQ.pop();
+    }
+    
+    while(!myQ.empty())
+    {
+        myQ.pop();
+    }
+    
+    return maskI[end.y * n + end.x];
 }
 
 /*for (int y = 0; y < m; y++)
